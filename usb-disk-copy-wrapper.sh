2761293487735883c6b79074c0224da552285ab1
Aleksey Ivanov <Aleksey.Ivanov@veeam.com>
2018-10-23 14:33:29 +0300

    edit: diagnostics messages

--- usb-disk-copy-wrapper.sh
+++ usb-disk-copy-wrapper.sh
@@ -20,33 +20,35 @@ LOG_FILE_BASE_NAME='debug' # Log file base name
 SCRIPT_NAME=`basename "$0"`
 echo ""
 echo "The script has started ($SCRIPT_NAME)"
+echo ""
 
 #### Checking settings ####
 echo "Checking settings..."
 # Nested script's path
 if [ -z "$SCRIPT_PATH" ]
 then
-    echo "ERROR: Nested script's path is not set"
+    echo "*** ERROR *** Nested script path is not set."
     exit 1
 else
-    echo "Nested script's path is '$SCRIPT_PATH'"
+    echo "Nested script path: $SCRIPT_PATH"
 fi
 
 # Log files' root folder
 if [ -z "$LOG_FILE_FOLDER" ]
 then
-    echo "WARNING: Log files' root folder is not set. Will use default path"
+    echo "*** WARNING *** Log files root folder is not set. Will use default path."
     $LOG_FILE_FOLDER='/var/log'
 fi
-echo "Log files' root folder path is '$LOG_FILE_FOLDER'"
+echo "Log files root folder path: $LOG_FILE_FOLDER"
 mkdir --parents $LOG_FILE_FOLDER
 
 # Log file's base name
 if [ -z "LOG_FILE_BASE_NAME" ]
 then
-    echo "WARNING: Log files' base name is not set. Will use default name"
+    echo "*** WARNING *** Log files base name is not set. Will use default name."
     $LOG_FILE_BASE_NAME='flash-dance'
 fi
+echo "Settings are checked."
 
 
 #### Generating log file name ####
@@ -54,18 +56,19 @@ echo "Generating log file name..."
 DATE=$(date +"%F_%H-%M-%S")
 if [ -z "$DATE" ]
 then
-    echo "WARNING: Unable to convert current date and time to string in order to use it in log file name. Will use failover log file name"
+    echo "*** WARNING *** Unable to convert current date and time to string in order to use it in log file name. Will use failover log file name."
     LOG_FILE="$LOG_FILE_FOLDER/$LOG_FILE_BASE_NAME.log"
 else
     LOG_FILE="$LOG_FILE_FOLDER/$LOG_FILE_BASE_NAME-$DATE.log"
 fi
-echo "Using log file '$LOG_FILE'."
+echo "Using log file: $LOG_FILE"
 
 
-echo "Starting nested script '$SCRIPT_PATH'..."
+echo ""
+echo "NESTED SCRIPT '$SCRIPT_PATH' IS STARTING..."
 "$SCRIPT_PATH" | tee "$LOG_FILE"
 
-echo "The script has run to it's end ($SCRIPT_NAME)"
 echo ""
+echo "NESTED SCRIPT '$SCRIPT_NAME' HAS FINISHED."
 
 exit 0
