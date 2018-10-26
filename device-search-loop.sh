2d520eef68d1f0e2a41ef8e4d6e862d0e7dedf24
vmkernel <Aleksey.I.Ivanov@gmail.com>
2018-10-26 23:52:08 +0300

    edited: scsi disks search logic

--- device-search-loop.sh
+++ device-search-loop.sh
@@ -6,32 +6,33 @@ ATTACHED_SCSI_DISKS=( $(ls -la /dev/ | grep -Pi "sd(\w+)1" | awk '{print $10}' |
 
 if [ ${#ATTACHED_SCSI_DISKS[*]} -le 0 ]
 then
-    echo "*** WARNING *** No devices not found. The script has terminated prematurely."
-    #exit 0
+    echo "*** WARNING *** No devices found. The script has terminated prematurely."
+    exit 0
 else
     echo "Found ${#ATTACHED_SCSI_DISKS[*]} device(s): ${ATTACHED_SCSI_DISKS[*]}"
 fi
 
 if [ ${#ATTACHED_SCSI_DISKS[*]} -lt 2 ]
 then
-    echo "*** WARNING *** Not enough devices to start copying. The script has terminated prematurely."
-    #exit 0
-else # Attached disks count greather than 2
+    echo "*** WARNING *** Not enough devices. Need at least two devices to start a copying process. The script has terminated prematurely."
+    exit 0
+else 
+    # Attached disks count greather than 2
     DST_DEVICE_NAME=${ATTACHED_SCSI_DISKS[0]}
     if [ -z "$DST_DEVICE_NAME" ]
     then
-        echo "*** ERROR *** Destination device name is not set. Check settings! The script has terminated unexpectedly."
+        echo "*** ERROR *** Unable to get destination device name. The script has terminated unexpectedly."
         exit 1
     else
-        echo "Found the destination device: /dev/$DST_DEVICE_NAME"
+        echo "Destination device: /dev/$DST_DEVICE_NAME"
     fi
 
     SRC_DEVICE_NAME=${ATTACHED_SCSI_DISKS[1]}
     if [ -z "$SRC_DEVICE_NAME" ]
     then
-        echo "*** ERROR *** Source device name is not set. Check settings! The script has terminated unexpectedly."
+        echo "*** ERROR *** Unable to get source device name. The script has terminated unexpectedly."
         exit 1
     else
-        echo "Found the source device: /dev/$SRC_DEVICE_NAME"  
+        echo "Source device: /dev/$SRC_DEVICE_NAME"  
     fi
 fi
