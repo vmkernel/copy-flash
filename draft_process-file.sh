986b3c25740a12e9d31164ab8f28daaf463810f6
Aleksey Ivanov <Aleksey.Ivanov@veeam.com>
2018-11-30 20:00:04 +0300

    fixed bugs, added a return code for 'both files are the same'

--- draft_process-file.sh
+++ draft_process-file.sh
@@ -1,6 +1,7 @@
 # Return values
 #   0 – no collision has been detected
-#   1 – a collision has been detected
+#   1 – a collision has been detected, files are different
+#   2 - a collision has been detected, but both files are the same
 #   -1 – an internal error has occured, can't check for collision
 #
 # Usage:
@@ -14,21 +15,21 @@ function check_files_collision () {
     # Checking first parameter
     if [ -z "$SRC_FOLDER_PATH" ]
     then
-        echo "*** ERROR *** Insufficient parameters: expected 3, got 0."
+        echo "*** ERROR *** Insufficient arguments: expected 3, got 0."
         return -1
     fi
     
     # Checking second parameter
     if [ -z "$DST_FOLDER_PATH" ]
     then
-        echo "*** ERROR *** Insufficient parameters: expected 3, got 1."
+        echo "*** ERROR *** Insufficient arguments: expected 3, got 1."
         return -1
     fi
 
     # Checking third parameter
     if [ -z "$SRC_FILE_NAME" ]
     then
-        echo "*** ERROR *** Insufficient parameters: expected 3, got 2."
+        echo "*** ERROR *** Insufficient arguments: expected 3, got 2."
         return -1
     fi
 
@@ -36,8 +37,8 @@ function check_files_collision () {
     SRC_FOLDER_RECORD=$(ls --all "$SRC_FOLDER_PATH" 2> /dev/null)
     if [ -z "$SRC_FOLDER_RECORD" ]
     then
-        echo "*** WARNING **** Source folder doesn't exists. Assuming no collission."
-        return 0
+        echo "*** ERROR **** Source folder doesn't exists. Input argument error."
+        return -1
     fi
 
     # Checking if the destiantion folder exists
@@ -80,10 +81,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_SIZE -ne $SRC_FILE_SIZE ]
         then
-            echo "Collision detected by file size: source = $SRC_FILE_SIZE, destination = $DST_FILE_SIZE."
-            #return 1
-        else
-            echo "Size: $SRC_FILE_SIZE (same)"
+            echo "A collision has been detected by file size (source = $SRC_FILE_SIZE, destination = $DST_FILE_SIZE)."
+            return 1
+        #else
+        #    echo "Size: $SRC_FILE_SIZE (same)"
         fi
 
         # Extracting date and time strings for both source and destination files
@@ -102,10 +103,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_YEAR -ne $SRC_FILE_YEAR ]
         then
-            echo "Collision detected by file year: source = $SRC_FILE_YEAR, destination = $DST_FILE_YEAR."
-            #return 1
-        else
-            echo "Year: $SRC_FILE_YEAR (same)"
+            echo "A collision has been detected by file year (source = $SRC_FILE_YEAR, destination = $DST_FILE_YEAR)."
+            return 1
+        #else
+        #    echo "Year: $SRC_FILE_YEAR (same)"
         fi
 
         # Comparing files month
@@ -118,10 +119,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_MONTH -ne $SRC_FILE_MONTH ]
         then
-            echo "Collision detected by file month: source = $SRC_FILE_MONTH, destination = $DST_FILE_MONTH."
-            #return 1
-        else
-            echo "Month: $SRC_FILE_MONTH (same)"
+            echo "A collision has been detected by file month (source = $SRC_FILE_MONTH, destination = $DST_FILE_MONTH)."
+            return 1
+        #else
+        #    echo "Month: $SRC_FILE_MONTH (same)"
         fi
 
         # Comparing files day
@@ -134,10 +135,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_DAY -ne $SRC_FILE_DAY ]
         then
-            echo "Collision detected by file day: source = $SRC_FILE_DAY, destination = $DST_FILE_DAY."
-            #return 1
-        else
-            echo "Day: $SRC_FILE_DAY (same)"
+            echo "A collision has been detected by file day (source = $SRC_FILE_DAY, destination = $DST_FILE_DAY)."
+            return 1
+        #else
+        #    echo "Day: $SRC_FILE_DAY (same)"
         fi
 
         # Comparing file hours
@@ -150,10 +151,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_HOURS -ne $SRC_FILE_HOURS ]
         then
-            echo "Collision detected by file hours: source = $SRC_FILE_HOURS, destination = $DST_FILE_HOURS."
-            #return 1
-        else
-            echo "Hour: $SRC_FILE_HOURS (same)"
+            echo "A collision has been detected by file hours (source = $SRC_FILE_HOURS, destination = $DST_FILE_HOURS)."
+            return 1
+        #else
+        #    echo "Hour: $SRC_FILE_HOURS (same)"
         fi
 
         # Comparing file minutes
@@ -166,10 +167,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_MINUTES -ne $SRC_FILE_MINUTES ]
         then
-            echo "Collision detected by file minutes: source = $SRC_FILE_MINUTES, destination = $DST_FILE_MINUTES."
-            #return 1
-        else
-            echo "Minute: $SRC_FILE_MINUTES (same)"
+            echo "A collision has been detected by file minutes (source = $SRC_FILE_MINUTES, destination = $DST_FILE_MINUTES)."
+            return 1
+        #else
+        #    echo "Minute: $SRC_FILE_MINUTES (same)"
         fi
 
         # Comparing file seconds 
@@ -182,10 +183,10 @@ function check_files_collision () {
         fi
         if [ $DST_FILE_SECONDS -ne $SRC_FILE_SECONDS ]
         then
-            echo "Collision detected by file seconds: source = $SRC_FILE_SECONDS, destination = $DST_FILE_SECONDS."
-            #return 1
-        else
-            echo "Seconds: $SRC_FILE_SECONDS (same)"
+            echo "A collision has been detected by file seconds (source = $SRC_FILE_SECONDS, destination = $DST_FILE_SECONDS)."
+            return 1
+        #else
+        #    echo "Seconds: $SRC_FILE_SECONDS (same)"
         fi
 
         ## Compating file milliseconds (I don't know for sure should it be here or not)
@@ -198,20 +199,20 @@ function check_files_collision () {
         #fi
         #if [ $DST_FILE_MILLISECONDS -ne $SRC_FILE_MILLISECONDS ]
         #then
-        #    echo "Collision detected by file milliseconds: source = $SRC_FILE_MILLISECONDS, destination = $DST_FILE_MILLISECONDS."
-        #    #return 1
-        #else
-        #    echo "Milliseconds: $SRC_FILE_MILLISECONDS (same)"
+        #    echo "A collision has been detected by file milliseconds (source = $SRC_FILE_MILLISECONDS, destination = $DST_FILE_MILLISECONDS)."
+        #    return 1
+        ##else
+        ##    echo "Milliseconds: $SRC_FILE_MILLISECONDS (same)"
         #fi
     fi
 
-    echo "No colissions has been detected."
-    return 0
+    echo "Both files are the same by their's date/time and size."
+    return 2
 }
 
 
 SRC_DEVICE_MOUNT_POINT="/home/pi/scripts/rpi-usb-disk-copy" # debug line
-DST_FOLDER_FULL_PATH="/home/pi/scripts" # debug line
+DST_FOLDER_FULL_PATH="/opt/usb-disk-copy" # debug line
 
 # Maximum value of file name counter
 #TODO: MOVE TO SETTINGS
@@ -238,26 +239,37 @@ do
         continue # BUG: Potential loss of data
     fi
 
+    # BUG: threats 'both files are the same' as no collistion
     check_files_collision "$SRC_DEVICE_MOUNT_POINT" "$DST_FOLDER_FULL_PATH" "$SRC_FILE_NAME"
     EXIT_CODE=$?
-    echo "Exit code: $EXIT_CODE"
+    echo "check_file_collison() has exited with code: $EXIT_CODE"
 
     declare -i IS_NEW_NAME_REQUIRED=1
+    declare -i IS_SKIP_FILE=0
     case $EXIT_CODE in
         0)
-            echo "No collision has been detected. Will simply copy the file to the destination."
+            echo "No collision has been detected. Will copy the file to its destination."
             IS_NEW_NAME_REQUIRED=0
+        ;;
+        2)
+            echo "A collision has been detected, but all files are the same. Will skip the file."
+            IS_SKIP_FILE=1
         ;;
         1)
             echo "A collision has been detected. Will copy the file with a new name."
         ;;
         -1)
-            echo "An internal error has occured in the detection mechanism. Will copy the file with a new name."
+            echo "An unknown internal error has occured in the detection mechanism. Will copy the file with a new name."
         ;;
         *)
-            echo "An error has occured in the detection mechanism: got unsupported return code. Will copy the file with a new name."
+            echo "An internal error has occured in the detection mechanism: got unsupported exit code ($EXIT_CODE). Will copy the file with a new name."
         ;;
     esac
+
+    if [ $IS_SKIP_FILE -eq 1 ]
+    then
+        continue
+    fi
 
     if [ $IS_NEW_NAME_REQUIRED -eq 1 ]
     then
@@ -287,7 +299,7 @@ do
         for FILE_NAME_COUNTER in `seq 1 $FILE_NAME_COUNTER_MAX`;
         do
             # Generating new file name
-            DST_FILE_NAME="$SRC_FILE_BASE_NAME ($FILE_NAME_COUNTER)"
+            DST_FILE_NAME="$SRC_FILE_BASE_NAME($FILE_NAME_COUNTER)"
             if [ $IS_NO_EXTENSION -ne 1 ]
             then
                 DST_FILE_NAME="$DST_FILE_NAME.$SRC_FILE_EXT"
@@ -306,7 +318,7 @@ do
             DST_FILE_RECORD=$(ls --all $DST_FILE_FULL_PATH 2> /dev/null)
             if [ -z $DST_FILE_RECORD ]
             then # Destination file with the new name is not found, will continue with the name
-                echo "Found first available name for the file: '$DST_FILE_NAME'"
+                echo "Found first available name: '$DST_FILE_NAME'"
                 IS_NEW_NAME_FOUND=1
                 break
             fi
