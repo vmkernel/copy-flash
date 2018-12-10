18de8ce3f4f7d56ec63a396a6b90f7482d2420f3
Aleksey Ivanov <Aleksey.Ivanov@veeam.com>
2018-12-10 19:03:23 +0300

    added checks, descriptions, comments; improved logics

--- draft_process-file.sh
+++ draft_process-file.sh
@@ -205,27 +205,34 @@ function check_files_collision () {
     return 2
 }
 
-# TODO: add a description for the function
 # TODO: rename the function
 function copy_files () {
     # SUMMARY
-    #
+    # This function discovers and copies all files from a specified folder to a destination folder
+    #   implementing extended logic to check if the destination has a file with the same name as the source one.
     #
     # DESCRIPTION
-    #
-    #
+    # If there's no file with the same name on the destination, the function simply copies the source file to the destination.
+    # If there's a file with the same name, the function performs some checks to determine should it skip the file (if both are the same)
+    #   or make a new name for the file prior calling rsync to copy the file.
+    # 
     # ARGUMENTS
-    #
+    # <source_folder> – full path to the source folder
+    # <destination_folder> – full path to the destination folder
     #
     # RETURN CODES
     #   0 – operation has been finished successfully
-    #  -1 – an internal error has occured, can't check for collision
+    #   1 – operation has been finished with errors
+    #   2 – operation has been finished with warnings
     #
     # USAGE
-    #
+    #   copy_files <source_folder> <destiantion_folder>
 
     SRC_FOLDER_PATH=$1 # Assuming the first parameter as a source folder
     DST_FOLDER_PATH=$2 # Assuming the second parameter as a destination folder
+
+    declare -i IS_ERROR_RECOVERABLE=0 # Flag to decide which exit code should return the function upon completion
+    declare -i IS_WARNING=0 # Flag to decide which exit code should return the function upon completion
 
     # Checking source folder path
     if [ -z "$SRC_FOLDER_PATH" ]
@@ -258,66 +265,99 @@ function copy_files () {
         DST_FOLDER_RECORD=$(ls --all "$DST_FOLDER_PATH" 2> /dev/null)
         if [ -z "$DST_FOLDER_RECORD" ]
         then
-            echo "*** ERROR **** Unable to create the destination filder."
+            echo "*** ERROR **** Unable to create the destination folder."
             return -1
         fi
     fi
 
+    # Discoverying files in the source folder
     SRC_FILES_LIST=( $(find $SRC_FOLDER_PATH -type f,l) )
-    # TODO: check SRC_FILES_LIST
-    echo "Found ${#SRC_FILES_LIST[*]} file(s)"
+    if [ ${#SRC_FILES_LIST[*]} -le 0 ]
+    then
+        echo "*** WARNING *** No files has been found in the source directory."
+        return 1
+    else
+        echo "Found ${#SRC_FILES_LIST[*]} file(s)"
+    fi
+
+    # Processing each of the discovered files
     for SRC_FILE_PATH in "${SRC_FILES_LIST[@]}"
     do
-        # TODO: check SRC_FILE_PATH
+        # Checking for an empty string
+        if [ -z "$SRC_FILE_PATH" ]
+        then
+            echo "*** ERROR *** Got an empty path from discovered files array instead of a file path. Will skip this line."
+            IS_ERROR_RECOVERABLE=1
+            continue # BUG: Potential loss of data
+        fi
+
         echo ""
         echo "Processing file '$SRC_FILE_PATH'"
 
+        # Checking if the source file exists
+        SRC_FILE_RECORD=$(ls --all "$SRC_FILE_PATH" 2> /dev/null)
+        if [ -z "$SRC_FILE_RECORD" ]
+        then
+            echo "*** ERROR **** Unable to find the source file '$SRC_FILE_PATH'. Will skip this one."
+            IS_ERROR_RECOVERABLE=1
+            continue # BUG: Potential loss of data
+        fi
+
+        # Extracting file name from the file path
         SRC_FILE_NAME=$(basename $SRC_FILE_PATH)
         if [ -z "$SRC_FILE_NAME" ]
         then 
             echo "*** ERROR *** Unable to extract file name from the file path. Will skip this file."
+            IS_ERROR_RECOVERABLE=1
             continue # BUG: Potential loss of data (try mkstemp?)
         fi
 
+        # Running collision check
         check_files_collision "$SRC_FOLDER_PATH" "$DST_FOLDER_PATH" "$SRC_FILE_NAME"
         EXIT_CODE=$?
 
-        declare -i IS_NEW_NAME_REQUIRED=1
-        declare -i IS_SKIP_FILE=0
+        # Analyzing collision check's result
+        declare -i IS_NEW_NAME_REQUIRED=1 # New name is required for the file
+        declare -i IS_SKIP_FILE=0         # Both files are the same, will skip safely
         case $EXIT_CODE in
-            0)
+            0)  # No collisions, no file with the same name at the destination
                 echo "Will copy the file to its destination with the original name."
                 IS_NEW_NAME_REQUIRED=0
             ;;
-            2)
+            2)  # Both files are the same
                 echo "Will skip the file."
                 IS_SKIP_FILE=1
             ;;
-            1)
+            1)  # Need a new name for the file, because of a collision
                 echo "Will copy the file with a new name."
             ;;
-            -1)
+            -1) # Internal error detected, assuming that new name is required
                 echo "An unknown internal error has occured in collisions detection mechanism. Will copy the file with a new name."
+                IS_WARNING=1
             ;;
-            *)
+            *)  # Internal error, unsopportex exit code, assuming that new name is required
                 echo "An internal error has occured in collisions detection mechanism: got unsupported exit code ($EXIT_CODE). Will copy the file with a new name."
+                IS_WARNING=1
             ;;
         esac
 
+        # Skipping the file if both files are the same
         if [ $IS_SKIP_FILE -eq 1 ]
         then
             continue
         fi
 
+        # Generating a brand new name for the file, if required
         if [ $IS_NEW_NAME_REQUIRED -eq 1 ]
         then
+            declare -i IS_NO_EXTENSION=0 # Flag, if there's no extension in the file
+            declare SRC_FILE_BASE_NAME   # Base name of the source file
+
             # Extracting base file name and extension from the file name
-            declare -i IS_NO_EXTENSION=0
-            declare SRC_FILE_BASE_NAME
             SRC_FILE_EXT="${SRC_FILE_NAME##*.}"
             if [ -z "$SRC_FILE_EXT" ]
             then
-                echo "*** WARNING *** The file has no extension."
+                #echo "*** WARNING *** The file has no extension."
                 IS_NO_EXTENSION=1
                 SRC_FILE_BASE_NAME=$SRC_FILE_NAME
             else
@@ -327,15 +367,18 @@ function copy_files () {
             if [ -z $SRC_FILE_BASE_NAME ]
             then
                 echo "*** ERROR *** Unable to extract base file name from the file name. Will skip the file."
+                IS_ERROR_RECOVERABLE=1
                 continue # BUG: Potential loss of data (try mkstemp?)
             fi
 
             # New file name generation algorithm (e.g.: <original_file_name><N>.<ext>)
             declare DST_FILE_NAME
-            declare -i IS_ERROR=0
+            declare -i IS_NAME_GEN_ERROR=0
             declare -i IS_NEW_NAME_FOUND=0
             for FILE_NAME_COUNTER in `seq 1 $FILE_NAME_COUNTER_MAX`;
             do
+                # TODO: check the counter & failover to a random value
+
                 # Generating new file name
                 DST_FILE_NAME="$SRC_FILE_BASE_NAME($FILE_NAME_COUNTER)"
                 if [ $IS_NO_EXTENSION -ne 1 ]
@@ -343,14 +386,14 @@ function copy_files () {
                     DST_FILE_NAME="$DST_FILE_NAME.$SRC_FILE_EXT"
                 fi
 
-                # TODO: check DST_FILE_NAME
+                # TODO: check DST_FILE_NAME for NULL
 
                 # Generating new file full path
                 DST_FILE_FULL_PATH="$DST_FOLDER_PATH/$DST_FILE_NAME"
                 if [ -z "$DST_FILE_FULL_PATH" ]
                 then
                     echo "*** ERROR *** Unable to generate destination file full path. Will skip the file."
-                    IS_ERROR=1
+                    IS_NAME_GEN_ERROR=1
                     break # BUG: Potential loss of data (try mkstemp?)
                 fi
 
@@ -364,8 +407,9 @@ function copy_files () {
                 fi
             done
 
-            if [ $IS_ERROR -ne 0 ]
+            if [ $IS_NAME_GEN_ERROR -ne 0 ]
             then
+                IS_ERROR_RECOVERABLE=1
                 continue # BUG: Potential loss of data (try mkstemp?)
             fi
 
@@ -373,7 +417,8 @@ function copy_files () {
             then
                 # TODO: Implement some failover mechanism to generata completely random file name
                 echo "*** ERROR *** The new file name generation alghorithm has run to it's maximum file counter value ($FILE_NAME_COUNTER_MAX), but was unable to find a free number for the file name. Will skip the file."
-                continue
+                IS_ERROR_RECOVERABLE=1
+                continue # BUG: Potential loss of data (try mkstemp?)
             fi
         fi
 
@@ -382,8 +427,20 @@ function copy_files () {
         EXIT_CODE=$?
 
         # TODO: check EXIT_CODE
-
     done
+
+    # TODO: Analyze error flags
+    if [ IS_ERROR_RECOVERABLE -eq 1 ]
+    then
+        return 1 # some errors has been deteced
+    else
+        if [ IS_WARNING -eq 1 ]
+        then
+            return 2 # some warnings has been detected
+        fi
+    fi
+
+    return 0 # no issues has been detected
 }
 
 SRC_DEVICE_MOUNT_POINT="/home/pi/scripts/rpi-usb-disk-copy" # debug line
