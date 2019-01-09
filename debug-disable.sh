9b436680cce87c2e800c9647759eb383b76e86fa
Aleksey Ivanov <Aleksey.Ivanov@veeam.com>
2019-01-09 18:28:18 +0300

    Added diagnostics information to debugging mode enabler/disables scripts

--- debug-disable.sh
+++ debug-disable.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 rm /tmp/KILLSWITCH_USB_DISK_COPY
+echo "DEBUGGING MODE DISABLED. Kill-switch file has been removed."
--- debug-enable.sh
+++ debug-enable.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 touch /tmp/KILLSWITCH_USB_DISK_COPY
+echo "DEBUGGING MODE ENABLED. Kill-switch file has been created."
